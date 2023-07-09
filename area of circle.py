459af625b26cfefbf41887521bedbff1fa664489
shreyalakade <132871943+shreyalakade@users.noreply.github.com>
2023-07-09 12:51:13 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,15 @@
+import numpy as np
+r = float(input("Input the radius of the circle: "))
+print("The area of the circle with radius " + str(r) + " is: " + str(np.pi*r**2))
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      

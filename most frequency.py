76f5c5377ffe48831254f04f3879638d8e8f1f8a
shreyalakade <132871943+shreyalakade@users.noreply.github.com>
2023-07-16 11:11:55 +0530

    Add files via upload

---
+++ most frequency.py
@@ -0,0 +1,19 @@
+str=input("please enter a string") #Mississippi
+d1=dict()
+for a in str:
+    if a in d1:
+        d1[a]=d1[a]+1
+    else:
+        d1[a]=1
+print(d1)
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      

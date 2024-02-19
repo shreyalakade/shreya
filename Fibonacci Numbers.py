45da8164ba659aa9e9989da17a4f879b12852474
shreyalakade <132871943+shreyalakade@users.noreply.github.com>
2024-02-19 11:42:33 +0530

    Add files via upload

---
+++ Fibonacci Numbers.py
@@ -0,0 +1,24 @@
+n=int(input("enter the range:"))
+
+a=0
+
+b=1
+
+c=3
+
+print(a)
+
+print(b)
+
+
+for i in range(1,n+1):
+
+    while n>=c:
+
+        d=a+b
+
+        a,b=b,d
+
+        if d<=n:
+
+            print(d)

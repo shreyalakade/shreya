41d7ec01b8c547364b0db58246cb63516ccea5f4
shreyalakade <132871943+shreyalakade@users.noreply.github.com>
2024-02-19 12:11:53 +0530

    Create Set Operation.py

---
+++ Set Operation.py
@@ -0,0 +1,10 @@
+E = {0, 2, 4, 6, 8}; # Frist set 
+N = {1, 2, 3, 4, 5}; #second set
+# E and N are two Sets
+print("Union of E and N is",E | N) #Union of Sets
+
+print("Intersection of E and N is",E & N) # Intersection of Sets
+
+print("Difference of E and N is",E - N) # Difference of Sets
+
+print("Symmetric difference of E and N is",E ^ N) # Symmetric Difference of Sets
